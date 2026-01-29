3a89ec2a171db6de914b675f154b40ebf002f21a
tgkhang <tgkhang22@clc.fitus.edu.vn>
2026-01-29 18:55:01 +0700

    Test with an array + each

--- unit-test-react-typescript/src/utils/mapOrder.test.ts
+++ unit-test-react-typescript/src/utils/mapOrder.test.ts
@@ -10,6 +10,20 @@ describe('Unit test: mapOrder():', () => {
   it('should return [] when key is null', () => {
     expect(mapOrder([{ id: 1 }], [1, 2, 3], null as any)).toEqual([])
   })
+  it('shoudl push items with key not in orderArray to the end', () => {
+    const originalArray = [
+      { id: 1, name: 'A' },
+      { id: 2, name: 'B' },
+      { id: 4, name: 'D' },
+    ]
+    const orderArray = [2, 3, 1]
+    const result = mapOrder(originalArray, orderArray, 'id')
+    expect(result).toEqual([
+      { id: 2, name: 'B' },
+      { id: 1, name: 'A' },
+      { id: 4, name: 'D' },
+    ])
+  })
   it('should return ordered array based on orderArray', () => {
     const originalArray = [
       { id: 3, name: 'C' },
@@ -22,6 +36,18 @@ describe('Unit test: mapOrder():', () => {
       { id: 2, name: 'B' },
       { id: 3, name: 'C' },
       { id: 1, name: 'A' },
+    ])
+  })
+  it('should handle when all items are not in orderArray', () => {
+    const originalArray = [
+      { id: 5, name: 'E' },
+      { id: 6, name: 'F' },
+    ]
+    const orderArray = [1, 2, 3]
+    const result = mapOrder(originalArray, orderArray, 'id')
+    expect(result).toEqual([
+      { id: 5, name: 'E' },
+      { id: 6, name: 'F' },
     ])
   })
 })
---
+++ unit-test-react-typescript/src/utils/validateEmail.test.ts
@@ -0,0 +1,15 @@
+import { validateEmail } from '~/utils/validateEmail'
+
+describe('Unit test: validateEmail():', () => {
+  const cases: any[] = [
+    ['khangaa@gmail.com', true],
+    ['person1@', false],
+    ['@person2.com', false],
+    [{ email: 'demo@gmail.com' }, true], // ex case unexpected input
+  ]
+
+  it.each(cases)('validateEmail(%s) should return %s', (...args: any[]) => {
+    const [email, expected] = args
+    expect(validateEmail(email)).toBe(expected)
+  })
+})
