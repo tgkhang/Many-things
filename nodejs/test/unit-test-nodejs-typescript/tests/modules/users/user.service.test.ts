b4cc96d310d0693bc09a4b1e21383e2bf80c03fc
tgkhang <tgkhang22@clc.fitus.edu.vn>
2026-02-07 21:19:22 +0700

    change os

--- nodejs/test/unit-test-nodejs-typescript/tests/modules/users/user.service.test.ts
+++ nodejs/test/unit-test-nodejs-typescript/tests/modules/users/user.service.test.ts
@@ -1,8 +1,36 @@
+import { bcrypt } from 'bcryptjs'
+import { StatusCodes } from 'http-status-codes'
 
+import { UserRepo } from '~/modules/users/user.repo'
 import { UserService } from '~/modules/users/user.service'
+import { UserRole } from '~/modules/users/user.types'
+
+// mock library bcryptjs
+jest.mock('bcryptjs', () => ({
+  hash: jest.fn(),
+}))
+
+jest.mock('~/modules/users/user.repo',()=>({
+  UserRepo:{
+    findByEmail: jest.fn(),
+    create: jest.fn(),
+    list: jest.fn(),
+    findById: jest.fn(),
+  }
+}))
 
 describe('UserService', () => {
-  it('should expose list()', () => {
-    expect(typeof UserService.list).toBe('function')
+  describe('register()', () => {
+    it('should throw error 409 if email already exists', async () => {
+
+      ;(UserRepo.findByEmail as jest.Mock).mockResolvedValue({
+        id: 'existing-user-id',
+        email: 'admin@gmail.com',
+        
+    })
   })
+
+  describe('list()', () => { 
+
+   })
 })
